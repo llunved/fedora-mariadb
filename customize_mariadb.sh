e9e522772152847dbd0e45c28f0bce54dac1b26a
Daniel Riek <daniel@llnvd.io>
2021-02-10 21:28:08 -0500

    Fixing string issues

--- customize_mariadb.sh
+++ customize_mariadb.sh
@@ -23,7 +23,7 @@ if [ "$(mysql -Be "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = '${MARIAD
     mysql -e "CREATE DATABASE ${MARIADB_DB}"
     mysql -e "CREATE USER ${MARIADB_USER}@localhost IDENTIFIED BY '${MARIADB_PWD}'"
     mysql -e "GRANT ALL PRIVILEGES ON ${MARIADB_DB}.* TO ${MARIADB_USER}@localhost"
-    printf "MARIADB_USER=\"${MARIADB_USER}\" \n MARIADB_PWD=\"${MARIADB_PWD}\" \n MARIADB_DB=\"${MARIADB_DB}\" \n MARIADB_ROOTPWD=\"${MARIADB_ROOTPWD}\" \n" > /etc/mariadb/dbauth.txt
+    printf "MARIADB_USER=${MARIADB_USER} \n MARIADB_PWD=${MARIADB_PWD} \n MARIADB_DB=${MARIADB_DB} \n MARIADB_ROOTPWD=${MARIADB_ROOTPWD} \n" > /etc/mariadb/dbauth.txt
     chmod 600 /etc/mariadb/dbauth.txt
 fi
 
